ec120e0c4248eb5a2eb84418c73c6e3d7b3bffb9
C. Baldauf <corinna@finding-marbles.com>
2013-10-12 23:11:44 +0200

    Changed tracking code from Google Analytics to Piwik - Better privacy for site visitors

--- index.php
+++ index.php
@@ -552,18 +552,20 @@ function init() {
     </div>
 </div>
 
-<script>
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-31461838-1']);
-    _gaq.push(['_trackPageview']);
+<!-- Piwik -->
+<script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(["trackPageView"]);
+    _paq.push(["enableLinkTracking"]);
+
     (function() {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
+        var u=(("https:" == document.location.protocol) ? "https" : "http") + "://finding-marbles.com/piwik/";
+        _paq.push(["setTrackerUrl", u+"piwik.php"]);
+        _paq.push(["setSiteId", "3"]);
+        var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+        g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
     })();
 </script>
+<!-- End Piwik Code -->
 </body>
 </html>
