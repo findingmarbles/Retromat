d9fbd17836eef4376a464b4111303d20996662ce
Pierre Martin <pierre@occi-tech.com>
2013-12-01 18:46:26 +0100

    Translated interface in French

--- lang/index_fr.php
+++ lang/index_fr.php
@@ -1,38 +1,38 @@
 <?php
 
-$_lang['HTML_TITLE'] = 'Inspiration &amp; plans for (agile) retrospectives';
+$_lang['HTML_TITLE'] = 'Inspiration &amp; plans pour rétrospectives (agile)';
 
-$_lang['INDEX_PITCH'] = 'Planning your next <b>retrospective</b>? Get started with a random plan, tweak it, print it and share the URL. Or just browse around for new ideas!';
-$_lang['INDEX_PLAN_ID'] = 'Plan-ID:';
-$_lang['INDEX_BUTTON_SHOW'] = 'Show!';
-$_lang['INDEX_RANDOM_RETRO'] = 'New random retrospective plan';
-$_lang['INDEX_ALL_ACTIVITIES_FOR_PHASE'] = 'All activities for';
-$_lang['INDEX_MINI_ABOUT'] = 'Retr-O-Mat contains <span id="footer_no_of_activities"></span> activities, allowing for <span id="footer_no_of_combinations"></span> combinations (<span id="footer_no_of_combinations_formula"></span>) and we are constantly adding more. You know a great activity?';
-$_lang['INDEX_MINI_ABOUT_SUGGEST'] = 'Suggest it';
+$_lang['INDEX_PITCH'] = 'Vous planifiez votre prochaine <b>rétrospective</b> ? Commencez avec un plan aléatoire, adaptez le, imprimez le et partagez son URL. Ou naviguez simplement pour avoir de nouvelles idées !';
+$_lang['INDEX_PLAN_ID'] = 'ID du plan :';
+$_lang['INDEX_BUTTON_SHOW'] = 'Afficher !';
+$_lang['INDEX_RANDOM_RETRO'] = 'Nouveau plan aléatoire de rétrospective';
+$_lang['INDEX_ALL_ACTIVITIES_FOR_PHASE'] = 'Toutes les activités pour';
+$_lang['INDEX_MINI_ABOUT'] = 'Retr-O-Mat contient <span id="footer_no_of_activities"></span> activités, permettant <span id="footer_no_of_combinations"></span> combinaisons (<span id="footer_no_of_combinations_formula"></span>) et nous en ajoutons sans cesse plus. Vous connaissez une activité géniale ?';
+$_lang['INDEX_MINI_ABOUT_SUGGEST'] = 'Suggérez la ';
 $_lang['INDEX_MINI_TEAM'] = <<<EOT
        <div style="margin-left:10px;">
            <img src="static/images/team/corinna_baldauf.jpg" width="70" height="93" title="Corinna Baldauf" class="mini-team-photo">
            <div class="mini-team-text" style="margin-right:0">
-           <h2>Created by <a href="http://finding-marbles.com/">Corinna Baldauf</a></h2>
-    Corinna wished for something like Retr-O-Mat during her Scrummaster years.
-    Eventually she just built it herself in the hope that it would be useful to others, too.
-    Any questions, suggestions or encouragement?
-    You can write her at <a href="mailto:corinna@finding-marbles.com">corinna@finding-marbles.com</a> or
-    <a href="https://twitter.com/findingmarbles">contact her on Twitter</a>.
+           <h2>Créé par <a href="http://finding-marbles.com/">Corinna Baldauf</a></h2>
+    Corinna souhaitait quelque chose comme Retr-O-Mat pendant ces années de Scrummaster.
+    Finalement elle le construit elle même dans l'espoir que cela serait utile à d'autres également.
+    Des questions, suggestions ou encouragements ?
+    Vous pouvez lui écrire à <a href="mailto:corinna@finding-marbles.com">corinna@finding-marbles.com</a> ou
+    <a href="https://twitter.com/findingmarbles">la contacter sur Twitter</a>.
            </div>
        </div>
 EOT;
 
-$_lang['PRINT_HEADER'] = 'by Finding-Marbles.com';
+$_lang['PRINT_HEADER'] = 'par Finding-Marbles.com';
 
-$_lang['ACTIVITY_SOURCE'] = 'Source:';
-$_lang['ACTIVITY_PREV'] = 'Show other activity for this phase';
-$_lang['ACTIVITY_NEXT'] = 'Show other activity for this phase';
-$_lang['ACTIVITY_PHOTO_ADD'] = 'Add Photo';
+$_lang['ACTIVITY_SOURCE'] = 'Source :';
+$_lang['ACTIVITY_PREV'] = 'Afficher une autre activité pour cette phase';
+$_lang['ACTIVITY_NEXT'] = 'Afficher une autre activité pour cette phase';
+$_lang['ACTIVITY_PHOTO_ADD'] = 'Àjouter une Photo';
 $_lang['ACTIVITY_PHOTO_MAIL_SUBJECT'] = 'Photos%20for%20Activity%3A%20ID';
 $_lang['ACTIVITY_PHOTO_MAIL_BODY'] = 'Hi%20Corinna%21%0D%0A%0D%0A[%20]%20Photo%20is%20attached%0D%0A[%20]%20Photo%20is%20online%20at%3A%20%0D%0A%0D%0ABest%2C%0D%0AYour%20Name';
 
-$_lang['ERROR_NO_SCRIPT'] = 'Retr-O-Mat relies heavily on JavaScript and doesn\'t work without it. Please enable JavaScript in your browser. Thanks!';
-$_lang['ERROR_MISSING_ACTIVITY'] = 'Sorry, can\'t find activity with ID';
+$_lang['ERROR_NO_SCRIPT'] = 'Retr-O-Mat est principalement basé sur JavaScript et ne fonctionne pas sans. Veuillez donc activer JavaScript dans votre navigateur. Merci !';
+$_lang['ERROR_MISSING_ACTIVITY'] = 'Désolé, impossible de trouver une activité avec l\'ID';
 
 ?>
