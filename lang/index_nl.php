a47d31f8ad613bce18bd21218c40a3187e3788f6
lvdpal <lvdpal@jduchess.org>
2014-01-10 14:32:05 +0100

    First part of Dutch translation (activities 1-15)

---
+++ lang/index_nl.php
@@ -0,0 +1,38 @@
+<?php
+
+$_lang['HTML_TITLE'] = 'Inspiratie &amp; plannen voor (agile) retrospectives';
+
+$_lang['INDEX_PITCH'] = 'Ben je bezig om je volgende <b>retrospective</b> voor te bereiden? Begin met een random plan, pas het aan, printen het uit en deel de URL. Of browse wat rond voor nieuwe ideeen!';
+$_lang['INDEX_PLAN_ID'] = 'Plan-ID:';
+$_lang['INDEX_BUTTON_SHOW'] = 'Toon!';
+$_lang['INDEX_RANDOM_RETRO'] = 'Nieuw random retrospective plan';
+$_lang['INDEX_ALL_ACTIVITIES_FOR_PHASE'] = 'Alle activiteiten voor';
+$_lang['INDEX_MINI_ABOUT'] = 'Retr-O-Mat bevat <span id="footer_no_of_activities"></span> activiteiten, die je in staat stellen om te <span id="footer_no_of_combinations"></span> combineren (<span id="footer_no_of_combinations_formula"></span>) en we zijn constant op zoek naar meer ideeen. Heb je nog een geweldigen activiteit toe te voegen?';
+$_lang['INDEX_MINI_ABOUT_SUGGEST'] = 'Doe een voorstel';
+$_lang['INDEX_MINI_TEAM'] = <<<EOT
+       <div style="margin-left:10px;">
+           <img src="static/images/team/corinna_baldauf.jpg" width="70" height="93" title="Corinna Baldauf" class="mini-team-photo">
+           <div class="mini-team-text" style="margin-right:0">
+           <h2>Created by <a href="http://finding-marbles.com/">Corinna Baldauf</a></h2>
+    Corinna wenste tijdens haar Scrummasterjaren vaak genoeg dat er iets als Retr-O-Mat was.
+    Uiteindelijk bouwde ze het zelf maar, in de hoop dat anderen het ook nuttig zouden vinden.
+    Heb je nog vragen, suggesties of aanmoedidingen?
+    Je kunt het haar schrijven op <a href="mailto:corinna@finding-marbles.com">corinna@finding-marbles.com</a> of
+    <a href="https://twitter.com/findingmarbles">contact opnemen via Twitter</a>.
+           </div>
+       </div>
+EOT;
+
+$_lang['PRINT_HEADER'] = 'door Finding-Marbles.com';
+
+$_lang['ACTIVITY_SOURCE'] = 'Bron:';
+$_lang['ACTIVITY_PREV'] = 'Toon een andere activiteit voor deze fase';
+$_lang['ACTIVITY_NEXT'] = 'Toon een andere activiteit voor deze fase';
+$_lang['ACTIVITY_PHOTO_ADD'] = 'Voeg een foto toe';
+$_lang['ACTIVITY_PHOTO_MAIL_SUBJECT'] = 'Photos%20for%20Activity%3A%20ID';
+$_lang['ACTIVITY_PHOTO_MAIL_BODY'] = 'Hi%20Corinna%21%0D%0A%0D%0A[%20]%20Photo%20is%20attached%0D%0A[%20]%20Photo%20is%20online%20at%3A%20%0D%0A%0D%0ABest%2C%0D%0AYour%20Name';
+
+$_lang['ERROR_NO_SCRIPT'] = 'Retr-O-Mat werkt niet zonder JavaScript. Enable alsjeblieft JavaScript in je browser als je de site wilt uitproberen.';
+$_lang['ERROR_MISSING_ACTIVITY'] = 'Sorry, er is geen activiteit met ID';
+
+?>
