13aea46ebc0663220d4330340e2da31f5907f659
C. Baldauf <corinna@finding-marbles.com>
2015-01-25 11:37:46 +0100

    (Belatedly) added photos.php - uups, sorry

---
+++ lang/photos.php
@@ -0,0 +1,123 @@
+
+var path = 'static/images/activities/';
+
+all_photos = [];
+all_photos[0] = [{
+    filename:    path + "1_ESVP.jpg",
+    contributor: "Reguel Wermelinger"
+}];
+all_photos[1] = [{
+filename:    path + "2_Weather-Report.jpg",
+contributor: "Philipp Flenker"
+}];
+all_photos[6] = [{
+filename:    path + "7_Mad-Sad-Glad.jpg",
+contributor: "Chloe Gachet"
+}];
+all_photos[8] = [{
+filename:    path + "9_Learning-Matrix.jpg",
+contributor: "Philipp Flenker"
+}];
+all_photos[11] = [{
+filename:    path + "12_Start-Stop-Continue.JPG",
+contributor: "Pedro Ángel Serrano"
+}];
+all_photos[18] = [{
+filename:    path + "19_Speedboat.jpg",
+contributor: "Corinna Baldauf"
+}];
+all_photos[19] = [
+{filename: path + "20_Perfection-Game.jpg", contributor: "Pieter Versteijnen"},
+{filename: path + "20_Perfection-Game-2.jpg", contributor: "Pedro Ángel Serrano"}
+];
+all_photos[20] = [{
+filename:    "http://1.bp.blogspot.com/-dLemopaMJ9o/UhKRRRBMFkI/AAAAAAAAC78/6hH5yQKucYA/s320/photo+4(1).JPG",
+contributor: "Paulo Caroli"
+}];
+all_photos[21] = [{
+filename:    path + "22_Temperature-Reading.jpg",
+contributor: "Weronika Kedzierska"
+}];
+all_photos[22] = [{
+filename:    path + "23_Feedback-Door-Smilies.jpg",
+contributor: "Philipp Flenker"
+}];
+all_photos[25] = [
+{filename:    path + "25_Cause-Effect-Diagramm.jpg", contributor: "Corinna Baldauf"},
+{filename: path + "25_Cause-Effect-Diagram-2.jpg", contributor: "Philipp Flenker"}
+];
+all_photos[30] = [{
+filename:    "31_Draw-Iteration.jpg",
+contributor: "Eric Lannemajou"
+}];
+all_photos[31] = [{
+filename:    path + "32_Emoticons.jpg",
+contributor: "Ruud Rietveld"
+}];
+all_photos[34] = [{
+filename:    path + "35_Agile-Self-Assessment.jpg",
+contributor: "Philipp Flenker"
+}];
+all_photos[41] = [{
+filename:    "http://findingmarblesdotcom.files.wordpress.com/2012/03/retrospective-with-postcards3.jpg",
+contributor: "Corinna Baldauf"
+}];
+all_photos[53] = [
+{filename:    path + "54_Story-Oscars.jpg", contributor: "Wolfgang Fassbender"},
+{filename:    path + "54_Story-Oscars_2.JPG", contributor: "Deborah Lindsay"},
+{filename:    path + "54_Story-Oscars_3.JPG", contributor: "Geert van der Ploeg"}
+];
+all_photos[58] = [
+{filename:    path + "59_Happiness-Histogram.jpg", contributor: "Weronika Kedzierska"},
+{filename:    path + "59_Happiness-Histogram-2.jpg", contributor: "Weronika Kedzierska"}
+];
+all_photos[59] = [{
+filename:    "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTrZZvgbLqG_3Tr5TOZr4HAuy0f4JlKtpoF5uOTe9HCtC3IbzIf",
+contributor: "Amber Haley"
+}];
+all_photos[60] = [{
+filename:    path + "61_Chaos-Cocktail-Party.jpg",
+contributor: "Philipp Flenker"
+}];
+all_photos[62] = [{
+filename:    path + "63_Low-Hanging-Fruit.jpg",
+contributor: "Pedro Ángel Serrano"
+}];
+all_photos[63] = [{
+filename:    "http://waynedgrant.files.wordpress.com/2012/08/quartering41.jpg?w=413&h=450",
+contributor: "Wayne D. Grant"
+}];
+all_photos[67] = [
+{filename:    path + "68_Company-Map.jpg", contributor: "Annette Harder"},
+{filename:    path + "68_Company-Map-2.JPG", contributor: "Pedro Ángel Serrano"}
+];
+all_photos[77] = [
+{filename:    "http://ebgconsulting.com/blog/wp-content/uploads/2010/06/4-Ls-poster-verticle-layout2.jpg", contributor: "Ellen Gottesdiener"},
+{filename:    path + "78_Four-Ls.jpg", contributor: "Philipp Flenker"},
+{filename:    path + "78_Four-Ls_2.jpg", contributor: "Geert van der Ploeg"}
+];
+all_photos[79] = [
+{filename:    path + "80_Repeat-Avoid.jpg", contributor: "Philipp Flenker"},
+{filename:    "http://4.bp.blogspot.com/-LLJU-U0lLFg/UR6j7F6mMnI/AAAAAAAACmU/P1NUW-KUraA/s1600/repeat-avoid.JPG", contributor: "Luis Goncalves"}
+];
+all_photos[80] = [{
+filename:    path + "81_Outcome-Expectations.jpg",
+contributor: "Philipp Flenker"
+}];
+all_photos[82] = [{
+filename:    path + "83_Retro-Dart.jpg",
+contributor: "Philipp Flenker"
+}];
+all_photos[88] = [
+{filename:    "http://scalablenotions.files.wordpress.com/2014/05/ideas.jpg?w=615", contributor: "Jordan Morris"},
+{filename:    path + "89_Retro-Wedding.JPG", contributor: "Pedro Ángel Serrano"}
+];
+all_photos[89] = [{
+filename:    path + "90_Agile-Values-Cheerup.jpg",
+contributor: "Daryl Praeker"
+}];
+all_photos[91] = [
+{filename:    path + "92_Motivational-Poster_INVEST.png", contributor: "Romain Trocherie"},
+{filename:    path + "92_Motivational-Poster_Pairing.png", contributor: "Romain Trocherie"}
+];
+
