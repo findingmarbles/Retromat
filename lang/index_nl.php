b5127c62b708a79bcdec24d0d6d465c2672189ca
lvdpal <lvanderpal@iprofs.nl>
2015-07-28 15:47:01 +0200

    Updated translation of index and changed profile picture file type

--- lang/index_nl.php
+++ lang/index_nl.php
@@ -6,21 +6,37 @@ $_lang['INDEX_PITCH'] = 'Ben je bezig om je volgende <b>retrospective</b> voor t
 $_lang['INDEX_PLAN_ID'] = 'Plan-ID:';
 $_lang['INDEX_BUTTON_SHOW'] = 'Toon!';
 $_lang['INDEX_RANDOM_RETRO'] = 'Nieuw random retrospective plan';
-$_lang['INDEX_ALL_ACTIVITIES_FOR_PHASE'] = 'Alle activiteiten voor';
-$_lang['INDEX_MINI_ABOUT'] = 'Retr-O-Mat bevat <span id="footer_no_of_activities"></span> activiteiten, die je in staat stellen om te <span id="footer_no_of_combinations"></span> combineren (<span id="footer_no_of_combinations_formula"></span>) en we zijn constant op zoek naar meer ideeen. Heb je nog een geweldigen activiteit toe te voegen?';
-$_lang['INDEX_MINI_ABOUT_SUGGEST'] = 'Doe een voorstel';
-$_lang['INDEX_MINI_TEAM'] = <<<EOT
-       <div style="margin-left:10px;">
-           <img src="static/images/team/corinna_baldauf.jpg" width="70" height="93" title="Corinna Baldauf" class="mini-team-photo">
-           <div class="mini-team-text" style="margin-right:0">
-           <h2>Created by <a href="http://finding-marbles.com/">Corinna Baldauf</a></h2>
-    Corinna wenste tijdens haar Scrummasterjaren vaak genoeg dat er iets als Retr-O-Mat was.
+$_lang['INDEX_ENTER_ID'] = 'Voer IDs om weer te geven in';
+$_lang['INDEX_SEARCH_KEYWORD'] = 'Zoek activities per steekwoord';
+$_lang['INDEX_ALL_ACTIVITIES'] = 'Alle activiteiten voor';
+$_lang['INDEX_LOADING'] = '... ACTIVITEITEN WORDEN GELADEN ...';
+
+$_lang['INDEX_NAVI_WHAT_IS_RETRO'] = '<a href="http://finding-marbles.com/retr-o-mat/what-is-a-retrospective/">What\'s a retrospective?</a>';
+$_lang['INDEX_NAVI_ABOUT'] = '<a href="http://finding-marbles.com/retr-o-mat/about-retr-o-mat/">About Retromat</a>';
+$_lang['INDEX_NAVI_PRINT'] = '<a href="/print/index.html">Print Edition</a>';
+$_lang['INDEX_NAVI_ADD_ACTIVITY'] = '<a href="https://docs.google.com/a/finding-marbles.com/spreadsheet/viewform?formkey=dEZZV1hPYWVZUDc2MFNsUEVRdXpMNWc6MQ">Add activity</a>';
+
+$_lang['INDEX_ABOUT'] = 'Retromat bevat <span class="js_footer_no_of_activities"></span> activiteiten, waarmee <span class="js_footer_no_of_combinations"></span> combinaties gemaakt kunnen worden(<span class="js_footer_no_of_combinations_formula"></span>) en er worden er steeds meer aan toegevoegd. Weet jij nog een geweldige activiteit?';
+$_lang['INDEX_ABOUT_SUGGEST'] = 'Stel het voor';
+
+$_lang['INDEX_TEAM_TRANSLATOR_TITLE'] = 'Vertaling: ';
+$_lang['INDEX_TEAM_TRANSLATOR_NAME'] = 'Linda van der Pal';
+$_lang['INDEX_TEAM_TRANSLATOR_LINK'] = '';
+$_lang['INDEX_TEAM_TRANSLATOR_IMAGE'] = 'static/images/team/lvdpal';
+$_lang['INDEX_TEAM_TRANSLATOR_TEXT'] = <<<EOT
+    Linda is Java ontwikkelaar en Scrum master bij <a href="http://iprofs.nl/">iProfs</a>. Ze is de oprichter van <a href="http://jduchess.org/">Duchess</a>
+	en een van de organisatoren van <a href="http://www.devoxx4kids.org/">Devoxx4Kids</a> in Nederland. Ze is groot fan van zowel Java als Agile conferenties 
+	en fanatiek Sketchnoter.
+	Je kunt haar vinden op <a href="https://twitter.com/DuchessFounder">Twitter</a>.
+EOT;
+
+$_lang['INDEX_TEAM_CORINNA_TITLE'] = 'Created by ';
+$_lang['INDEX_TEAM_CORINNA_TEXT'] = $_lang['INDEX_MINI_TEAM'] = <<<EOT
+    Corinna wenste tijdens haar Scrummasterjaren vaak genoeg dat er iets als Retromat  was.
     Uiteindelijk bouwde ze het zelf maar, in de hoop dat anderen het ook nuttig zouden vinden.
     Heb je nog vragen, suggesties of aanmoedidingen?
-    Je kunt het haar schrijven op <a href="mailto:corinna@finding-marbles.com">corinna@finding-marbles.com</a> of
-    <a href="https://twitter.com/findingmarbles">contact opnemen via Twitter</a>.
-           </div>
-       </div>
+    Je kunt het haar <a href="mailto:corinna@finding-marbles.com">mailen</a> of
+    <a href="https://twitter.com/findingmarbles">vertellen op Twitter</a>.
 EOT;
 
 $_lang['PRINT_HEADER'] = 'door Finding-Marbles.com';
@@ -31,8 +47,18 @@ $_lang['ACTIVITY_NEXT'] = 'Toon een andere activiteit voor deze fase';
 $_lang['ACTIVITY_PHOTO_ADD'] = 'Voeg een foto toe';
 $_lang['ACTIVITY_PHOTO_MAIL_SUBJECT'] = 'Photos%20for%20Activity%3A%20ID';
 $_lang['ACTIVITY_PHOTO_MAIL_BODY'] = 'Hi%20Corinna%21%0D%0A%0D%0A[%20]%20Photo%20is%20attached%0D%0A[%20]%20Photo%20is%20online%20at%3A%20%0D%0A%0D%0ABest%2C%0D%0AYour%20Name';
+$_lang['ACTIVITY_PHOTO_VIEW_PHOTO'] = 'Bekijk foto';
+$_lang['ACTIVITY_PHOTO_VIEW_PHOTOS'] = 'Bekijke foto\'s';
+$_lang['ACTIVITY_PHOTO_BY'] = 'Foto door ';
 
 $_lang['ERROR_NO_SCRIPT'] = 'Retr-O-Mat werkt niet zonder JavaScript. Enable alsjeblieft JavaScript in je browser als je de site wilt uitproberen.';
 $_lang['ERROR_MISSING_ACTIVITY'] = 'Sorry, er is geen activiteit met ID';
+
+$_lang['POPUP_CLOSE'] = 'Sluiten';
+$_lang['POPUP_IDS_BUTTON'] = 'Toon!';
+$_lang['POPUP_IDS_INFO']= 'Voorbeeld ID: 3-33-20-13-45';
+$_lang['POPUP_SEARCH_BUTTON'] = 'Zoeken';
+$_lang['POPUP_SEARCH_INFO']= 'Zoek op titel, samenvatting of beschrijving';
+$_lang['POPUP_SEARCH_NO_RESULTS'] = 'Helaas, er werd niets gevonden voor';
 
 ?>
