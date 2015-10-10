821a2230af97e1d18228a1671609bb2e92e48b4e
C. Baldauf <corinna@finding-marbles.com>
2015-10-10 22:09:22 +0200

    Added #112 and #113

--- index.php
+++ index.php
@@ -668,7 +668,7 @@ function switchLanguage(new_lang) {
 
     <select class="languageswitcher" onChange="switchLanguage(this.value)">
         <option value="de" <?php echo(print_if_selected("de", $lang)); ?> >Deutsch (28 Aktivit&auml;ten)</option>
-        <option value="en" <?php echo(print_if_selected("en", $lang)); ?> >English (111 activities)</option>
+        <option value="en" <?php echo(print_if_selected("en", $lang)); ?> >English (113 activities)</option>
         <option value="es" <?php echo(print_if_selected("es", $lang)); ?> >Espa&ntilde;ol (95 actividades)</option>
         <option value="fr" <?php echo(print_if_selected("fr", $lang)); ?> >Fran&ccedil;ais (47 activit&eacute;s)</option>
         <option value="nl" <?php echo(print_if_selected("nl", $lang)); ?> >Nederlands (46 activiteiten)</option>
