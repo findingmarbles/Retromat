d33f80f2ca87156c9a8bec3fdb22c2651882825e
C. Baldauf <corinna@finding-marbles.com>
2015-11-19 19:46:53 +0100

    Bugfix number typo

--- index.php
+++ index.php
@@ -670,8 +670,8 @@ function switchLanguage(new_lang) {
         <option value="de" <?php echo(print_if_selected("de", $lang)); ?> >Deutsch (36 Aktivit&auml;ten)</option>
         <option value="en" <?php echo(print_if_selected("en", $lang)); ?> >English (115 activities)</option>
         <option value="es" <?php echo(print_if_selected("es", $lang)); ?> >Espa&ntilde;ol (95 actividades)</option>
-        <option value="fr" <?php echo(print_if_selected("fr", $lang)); ?> >Fran&ccedil;ais (66 activit&eacute;s)</option>
-        <option value="nl" <?php echo(print_if_selected("nl", $lang)); ?> >Nederlands (46 activiteiten)</option>
+        <option value="fr" <?php echo(print_if_selected("fr", $lang)); ?> >Fran&ccedil;ais (47 activit&eacute;s)</option>
+        <option value="nl" <?php echo(print_if_selected("nl", $lang)); ?> >Nederlands (66 activiteiten)</option>
     </select>
 
       <span class="navi">
