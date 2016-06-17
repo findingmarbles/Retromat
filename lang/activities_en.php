878409bba2f0a13532d923fed9d071b70fdee8c0
Corinna <pallas@Corinnas-MacBook-Air.local>
2016-06-17 23:59:47 +0200

    Added #122 Positive and #123 Focus Principle

--- lang/activities_en.php
+++ lang/activities_en.php
@@ -1934,5 +1934,53 @@ source:    "<a href='http://qualityswdev.com/2016/02/04/wild-wild-west-retrospec
 duration:  "10 minutes",
 suitable:  "iteration, project"
 };
+all_activities[121] = {
+phase:     0,
+name:      "Positive and True",
+summary:   "Boost everyones energy with tailored questions",
+desc:      "Ask your neighbor a question that is tailored to get a response that \
+is positive, true and about their own experiences, e.g. \
+<ul> \
+<li>What have you done really well in the last iteration?</li> \
+<li>What is something that makes you really happy?</li> \
+<li>What were you most happy about yesterday?</li> \
+</ul> \
+Then your neighbor asks their neighbor on the other side the same question and \
+so on until everyone has answered and asked. \
+<br><br> \
+This will give everyone a boost and lead to better results.",
+source:    "<a href='http://sinnvoll-fuehren.com/veronika-kotrba/'>Veronika Kotrba</a> adapted it from <a href='http://www.timetothink.com/meet-us/nancy-kline/'>Nancy Kline</a>",
+duration:  "short",
+suitable:  "iteration, project, release"
+};
+all_activities[122] = {
+phase:     1,
+name:      "Find your Focus Principle",
+summary:   "Discuss the 12 agile principles and pick one to work on",
+desc:      "Print the <a href='http://www.agilemanifesto.org/principles.html'>principles of the Agile Manifesto</a> \
+onto cards, one principle \
+per card. If the group is large, split it and provide each smaller group with \
+their own set of the principles. \
+<br><br> \
+Explain that you want to order the principles according to the following question: \
+'How much do we need to improve regarding this principle?'. In the end the \
+principle that is the team's weakest spot should be on top of the list. \
+<br><br> \
+Start with a random principle, discuss what it means and how much need for \
+improvement you see, then place it in the middle. \
+Pick the next principle, discuss what it means and sort it relatively to the other \
+principles. You can propose a position depending on the previous discussion and \
+move from there by comparison. \
+Repeat this until all cards are sorted. \
+<br><br> \
+Now consider the card on top: This is presumeably the most needed and most urgent \
+principle you should work on. How does the team feel about it? Does everyone still \
+agree? What are the reasons there is the biggest demand for change here? Should you \
+compare to the second or third most important issue again? If someone would now \
+rather choose the second position, why?",
+source:    "<a href='http://www.agilesproduktmanagement.de/'>Tobias Baier</a>",
+duration:  "long",
+suitable:  "iteration, project, release"
+};
 // Values for duration: "<minMinutes>-<maxMinutes> perPerson"
 // Values for suitable: "iteration, realease, project, introverts, max10People, rootCause, smoothSailing, immature, largeGroup"
