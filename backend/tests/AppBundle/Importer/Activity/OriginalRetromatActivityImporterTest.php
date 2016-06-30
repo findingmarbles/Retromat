2fc5e41bf6f71c0f2e010405d18ba0070471779e
Timon Fiddike <timon.fiddike@marketvalid.com>
2016-06-30 14:56:17 +0200

    TDD: red: new tests for extractSource: Wow, for source: there are at least 5 cases: placeholder and string in any order, if source is the last property of the activity block, there may be no training comma.

--- backend/tests/AppBundle/Importer/Activity/OriginalRetromatActivityImporterTest.php
+++ backend/tests/AppBundle/Importer/Activity/OriginalRetromatActivityImporterTest.php
@@ -172,4 +172,105 @@ HTML;
 
         $this->assertEquals($expected, $this->importer->extractActivityDescription($activityBlock));
     }
+
+    public function testExtractSourcePlaceholderLastLine()
+    {
+        $activityBlock = <<<'HTML'
+phase:     0,
+name:      "Temperature Reading",
+summary:   "Participants mark their 'temperature' (mood) on a flipchart",
+desc:      "Prepare a flipchart with a drawing of a thermometer from freezing to body temperature to hot. \
+Each participant marks their mood on the sheet.",
+source:  source_unknown,
+HTML;
+
+        $this->assertEquals('source_unknown', $this->importer->extractActivitySource($activityBlock));
+    }
+    public function testExtractSourcePlaceholderLastLineNoComma()
+    {
+        $activityBlock = <<<'HTML'
+phase:     0,
+name:      "Check In - Quick Question", // TODO This can be expanded to at least 10 different variants - how?
+summary:   "Ask one question that each participant answers in turn",
+desc:      "In round-robin each participant answers the same question (unless they say 'I pass'). \
+Sample questions: <br>\
+<ul>\
+    <li>In one word - What do you need from this retrospective?</li>\
+Address concerns, e.g. by writing it down and setting it - physically and mentally - aside</li>\
+    <li>In this retrospective - If you were a car, what kind would it be?</li>\
+    <li>What emotional state are you in (e.g. 'glad', 'mad', 'sad', 'scared'?)</li>\
+</ul><br>\
+Avoid evaluating comments such as 'Great'. 'Thanks' is okay.",
+source:  source_agileRetrospectives
+HTML;
+
+        $this->assertEquals('source_agileRetrospectives', $this->importer->extractActivitySource($activityBlock));
+    }
+
+    public function testExtractSourceStringAndPlaceholder()
+    {
+        $activityBlock = <<<'HTML'
+phase:     4,
+name:      "Feedback Door - Numbers",
+summary:   "Gauge participants' satisfaction with the retro on a scale from 1 to 5 in minimum time",
+desc:      "Put sticky notes on the door with the numbers 1 through 5 on them. 1 is the topmost and best, \
+5 the lowest and worst.\
+When ending the retrospective, ask your participants to put a sticky to the number they feel \
+reflects the session. The sticky can be empty or have a comment or suggestion on it.",
+source:    "ALE 2011, " + source_findingMarbles,
+duration:  "2-3",
+suitable: "iteration, largeGroups"
+HTML;
+
+        $expected = <<<'HTML'
+"ALE 2011, " + source_findingMarbles
+HTML;
+
+        $this->assertEquals($expected, $this->importer->extractActivitySource($activityBlock));
+    }
+
+    public function testExtractSourcePlaceholderAndString()
+    {
+        $activityBlock = <<<'HTML'
+phase:     4,
+name:      "Appreciations",
+summary:   "Let team members appreciate each other and end positively",
+desc:      "Start by giving a sincere appreciation of one of the participants. \
+It can be anything they contributed: help to the team or you, a solved problem, ...\
+Then invite others and wait for someone to work up the nerve. Close, when no one \
+has talked for a minute.",
+source:    source_agileRetrospectives + " who took it from 'The Satir Model: Family Therapy and Beyond'",
+duration:  "5-30 groupsize",
+suitable: "iteration, release, project"
+HTML;
+
+        $expected = <<<'HTML'
+source_agileRetrospectives + " who took it from 'The Satir Model: Family Therapy and Beyond'"
+HTML;
+
+        $this->assertEquals($expected, $this->importer->extractActivitySource($activityBlock));
+    }
+
+    public function testExtractSourceString()
+    {
+        $activityBlock = <<<'HTML'
+phase:     4, // 5 geht auch
+name:      "SaMoLo (More of, Same of, Less of)",
+summary:   "Get course corrections on what you do as a facilitator",
+desc:      "Divide a flip chart in 3 sections titled 'More of', 'Same of', and 'Less of'. \
+Ask participants to nudge your behaviour into the right direction: Write stickies \
+with what you should do more, less and what is exactly right. Read out and briefly \
+discuss the stickies section-wise.",
+source:    "<a href='http://fairlygoodpractices.com/samolo.htm'>Fairly good practices</a>",
+more:      "<a href='http://www.scrumology.net/2010/05/11/samolo-retrospectives/'>David Bland's experiences</a>",
+duration:  "5-10",
+suitable: "iteration, release, project"
+HTML;
+
+        $expected = <<<'HTML'
+"<a href='http://fairlygoodpractices.com/samolo.htm'>Fairly good practices</a>"
+HTML;
+
+        $this->assertEquals($expected, $this->importer->extractActivitySource($activityBlock));
+    }
 }
