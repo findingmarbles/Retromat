59385d260cd37877c960b120a93be6077da40758
Timon Fiddike <timon.fiddike@marketvalid.com>
2016-07-04 22:19:04 +0200

    TDD: red: ArrayToObjectMapper->fillObjectFromArray()

---
+++ backend/tests/AppBundle/Importer/ArrayToObjectMapperTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace tests\AppBundle\Importer;
+
+use AppBundle\Importer\ArrayToObjectMapper;
+
+class ArrayToObjectMapperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFillObjectFromArray()
+    {
+        $mapper = new ArrayToObjectMapper();
+        $inputArray = [
+            'firstDummyProperty' => 23,
+            'SecondDummyPROPERTY' => 'FortyTwo',
+            'NonExistingProperty' => 'ToBeSkippedWithoutError',
+        ];
+        $objectToFill = new FillMeDummy();
+        $mapper->fillObjectFromArray($inputArray, $objectToFill);
+
+        $this->assertEquals(23, $objectToFill->getFirstDummyProperty());
+        $this->assertEquals('FortyTwo', $objectToFill->getSecondDummyProperty());
+    }
+}
+
+class FillMeDummy
+{
+    private $firstDummyProperty;
+
+    private $secondDummyProperty;
+
+    public function getFirstDummyProperty()
+    {
+        return $this->firstDummyProperty;
+    }
+
+    public function getSecondDummyProperty()
+    {
+        return $this->secondDummyProperty;
+    }
+
+    public function setFirstDummyProperty($firstDummyProperty)
+    {
+        $this->firstDummyProperty = $firstDummyProperty;
+    }
+
+    public function setSecondDummyProperty($secondDummyProperty)
+    {
+        $this->secondDummyProperty = $secondDummyProperty;
+    }
+}
