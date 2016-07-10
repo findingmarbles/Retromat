2fa299694ee1a195bdc4f7badd847160f674dbba
Timon Fiddike <timon.fiddike@marketvalid.com>
2016-07-10 11:28:43 +0200

    Installed Liip Functional Test Bundle, see: https://github.com/liip/LiipFunctionalTestBundle

--- backend/app/AppKernel.php
+++ backend/app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new Liip\FunctionalTestBundle\LiipFunctionalTestBundle();
         }
 
         return $bundles;
