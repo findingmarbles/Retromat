3e2952a90c6579f0a9fbb81c532728ebcdab99e8
Timon Fiddike <timon.fiddike@marketvalid.com>
2016-07-14 20:58:03 +0200

    keep URLs identical to original retromat but internally give templates consistent names including the language, i.e. index_en.html.twig

--- backend/src/AppBundle/Controller/HomeController.php
+++ backend/src/AppBundle/Controller/HomeController.php
@@ -15,6 +15,6 @@ class HomeController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index_en.html.twig');
     }
 }
