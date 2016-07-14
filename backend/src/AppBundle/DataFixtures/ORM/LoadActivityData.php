7f2c2375f2dc65b1f2fc4d776e2f61c190fcc1ea
Timon Fiddike <timon.fiddike@marketvalid.com>
2016-07-14 02:03:00 +0200

    let the container construct the ActivityImporter for us, this makes the fixture nice and short :-)

--- backend/src/AppBundle/DataFixtures/ORM/LoadActivityData.php
+++ backend/src/AppBundle/DataFixtures/ORM/LoadActivityData.php
@@ -2,17 +2,10 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-use AppBundle\Entity\Activity;
-use AppBundle\Importer\Activity\ActivityImporter;
-use AppBundle\Importer\Activity\ActivityReader;
-use AppBundle\Importer\ArrayToObjectMapper;
-use AppBundle\Importer\EntityCollectionFilter;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class LoadActivityData implements FixtureInterface, ContainerAwareInterface
 {
@@ -28,19 +21,7 @@ class LoadActivityData implements FixtureInterface, ContainerAwareInterface
 
     public function load(ObjectManager $manager)
     {
-        $reader = new ActivityReader($activityFileName = __DIR__.'/../../../../../lang/activities_en.php');
-
-        $mapper = new ArrayToObjectMapper();
-
-        /** @var ValidatorInterface $validator */
-        $validator = $this->container->get('validator');
-
-        /** @var LoggerInterface $logger */
-        $logger = $this->container->get('logger');
-        $filter = new EntityCollectionFilter($validator, $logger);
-
-        $activityImporter = new ActivityImporter($reader, $mapper, $filter);
-        $activities = $activityImporter->import();
+        $activities = $this->container->get('retromat.activity_importer')->import();
 
         foreach ($activities as $activity) {
             $manager->persist($activity);
