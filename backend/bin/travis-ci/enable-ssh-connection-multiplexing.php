7ccf70d138c5222bd67dcf5a0273c5d2fab8327e
Timon Fiddike <timon.fiddike@marketvalid.com>
2016-11-22 23:11:18 +0100

    shutdown experiment did not work, maybe try different ones later. Chose a short idle timeout for now.

--- backend/bin/travis-ci/enable-ssh-connection-multiplexing.php
+++ backend/bin/travis-ci/enable-ssh-connection-multiplexing.php
@@ -5,7 +5,7 @@ Host vega.uberspace.de
 \tStrictHostKeyChecking no
 \tControlMaster auto
 \tControlPath ~/.ssh/master-%r@%h:%p
-\tControlPersist 2m
+\tControlPersist 15
 ";
 
 file_put_contents(getenv('HOME').'/.ssh/config', $config, FILE_APPEND);
