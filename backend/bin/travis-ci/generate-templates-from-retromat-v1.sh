4763991d2a1cbc5261750f5b77ebb48c78c28fa3
Timon Fiddike <timon.fiddike@marketvalid.com>
2016-12-22 18:54:16 +0100

    Generate Twig templates for v2 from retromat v1 and clear cache.

--- backend/bin/travis-ci/generate-templates-from-retromat-v1.sh
+++ backend/bin/travis-ci/generate-templates-from-retromat-v1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-php index.php en > backend/app/Resources/views/home/generated/index_en.html.twig
-php index.php de > backend/app/Resources/views/home/generated/index_de.html.twig
-php index.php es > backend/app/Resources/views/home/generated/index_es.html.twig
-php index.php fr > backend/app/Resources/views/home/generated/index_fr.html.twig
-php index.php nl > backend/app/Resources/views/home/generated/index_nl.html.twig
+
+php index.php en twig   > backend/app/Resources/views/home/generated/index_en.html.twig
+php index.php de        > backend/app/Resources/views/home/generated/index_de.html.twig
+php index.php es        > backend/app/Resources/views/home/generated/index_es.html.twig
+php index.php fr        > backend/app/Resources/views/home/generated/index_fr.html.twig
+php index.php nl        > backend/app/Resources/views/home/generated/index_nl.html.twig
+
+php backend/bin/console cache:clear --env=prod
