30e3c4f78601e839c80d2ff0ba3555117eb9c2be
Timon Fiddike <timon.fiddike@marketvalid.com>
2016-12-26 14:45:54 +0100

    Avoid integration test by mocking the repository. More test code, but much faster and independent of the database. Ready for new activities to be added :-)

--- backend/tests/AppBundle/Twig/ActivityByPhaseTest.php
+++ backend/tests/AppBundle/Twig/ActivityByPhaseTest.php
@@ -3,10 +3,11 @@
 namespace tests\AppBundle\Twig;
 
 use AppBundle\Twig\ActivityByPhase;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 
-// @Todo: Avoid integration test by mocking the repository
-class ActivityByPhaseIntegrationTest extends WebTestCase
+class ActivityByPhaseTest extends WebTestCase
 {
     /**
      * @var ActivityByPhase
@@ -14,8 +15,32 @@ class ActivityByPhaseIntegrationTest extends WebTestCase
     private $activityByPhase;
 
     public function setUp() {
-        $this->loadFixtures(['AppBundle\DataFixtures\ORM\LoadActivityData']);
-        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $activityByPhase = [
+            0 => [1, 2, 3, 18, 22, 31, 32, 36, 42, 43, 46, 52, 59, 70, 76, 81, 82, 84, 85, 90, 106, 107, 108, 114, 122],
+            1 => [4, 5, 6, 7, 19, 33, 35, 47, 51, 54, 62, 64, 65, 75, 78, 79, 80, 86, 87, 89, 93, 97, 98, 110, 116, 119, 121, 123, 126, 127],
+            2 => [8, 9, 10, 20, 25, 26, 37, 41, 50, 55, 58, 66, 68, 69, 74, 91, 94, 95, 105, 113, 115, 118],
+            3 => [11, 12, 13, 21, 24, 29, 38, 39, 48, 49, 61, 63, 72, 73, 88, 96, 99, 100, 103, 117, 124, 125],
+            4 => [14, 15, 16, 17, 23, 34, 40, 44, 45, 53, 57, 60, 67, 71, 77, 83, 92, 101, 102, 104, 109, 112, 120],
+            5 => [27, 28, 30, 56, 111],
+        ];
+
+        $activityRepository = $this
+            ->getMockBuilder(EntityRepository::class)
+            ->setMethods(['findAllActivitiesByPhases'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $activityRepository->expects($this->any())
+            ->method('findAllActivitiesByPhases')
+            ->will($this->returnValue($activityByPhase));
+
+        $entityManager = $this
+            ->getMockBuilder(EntityManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $entityManager->expects($this->any())
+            ->method('getRepository')
+            ->will($this->returnValue($activityRepository));
+
         $this->activityByPhase = new ActivityByPhase($entityManager);
     }
 
