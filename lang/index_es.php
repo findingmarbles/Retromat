2c72be2191ad7a70ba3e2fd07abbab65583e0552
paserrano <pserrano@gmail.com>
2016-12-29 15:57:31 +0100

    Added Timon's profile translation

--- lang/index_es.php
+++ lang/index_es.php
@@ -1,52 +1,75 @@
 <?php
 
-$_lang['HTML_TITLE'] = 'Inspiration &amp; plans for (agile) retrospectives';
+$_lang['HTML_TITLE'] = 'Inspiraci&oacute;n &amp; planes para retrospectivas (&aacute;giles)';
 
-$_lang['INDEX_PITCH'] = '¿Estas planeando tu siguiente <b>retrospectiva</b>? Comienza con una configuración aleatoria, modificala, imprimela y comparte la url. O simplemente navega para ver nuevas ideas!';
+$_lang['INDEX_PITCH'] = '&iquest;Est&aacute;s planificando tu pr&oacute;xima <b>retrospectiva</b>?  Empieza con un plan aleatorio, modif&iacute;calo, impr&iacute;melo y comparte su URL. O simplemente navega en b&uacute;squeda de nuevas ideas.';
 $_lang['INDEX_PLAN_ID'] = 'IDs:';
-$_lang['INDEX_BUTTON_SHOW'] = 'Mostrar!';
-$_lang['INDEX_RANDOM_RETRO'] = 'Nuevo plan de retrosctiva aleatorio';
-$_lang['INDEX_ENTER_ID'] = 'Introduce IDs para visualizar';
+$_lang['INDEX_BUTTON_SHOW'] = 'Mostrar';
+$_lang['INDEX_RANDOM_RETRO'] = 'Nuevo plan de retrospectiva aleatorio';
+$_lang['INDEX_ENTER_ID'] = 'Ingresar IDs a visualizar';
 $_lang['INDEX_SEARCH_KEYWORD'] = 'Buscar actividades por palabra clave';
 $_lang['INDEX_ALL_ACTIVITIES'] = 'Todas las actividades para';
+$_lang['INDEX_LOADING'] = '... LOADING ACTIVITIES ...';
 
-$_lang['INDEX_ABOUT'] = 'Retr-O-Mat contiene <span class="js_footer_no_of_activities"></span> actividades, pudiendo generar <span class="js_footer_no_of_combinations"></span> combinaciones (<span class="js_footer_no_of_combinations_formula"></span>) y nosotros estamos continuamente añadiendo mas. ¿Conoces una posible actividad?';
-$_lang['INDEX_ABOUT_SUGGEST'] = 'Cuentanosla!';
+$_lang['INDEX_NAVI_WHAT_IS_RETRO'] = '<a href="http://thomaswallet.blogspot.com.ar/2014/10/que-es-una-retrospectiva.html">¿Qué es una retrospectiva?</a>';
+$_lang['INDEX_NAVI_ABOUT'] = '<a href="http://thomaswallet.blogspot.com.ar/2014/09/retr-o-mat-en-espanol.html">Sobre Retromat</a>';
+$_lang['INDEX_NAVI_PRINT'] = '<a href="/print/index.html">Print Edition</a>';
+$_lang['INDEX_NAVI_ADD_ACTIVITY'] = '<a href="https://docs.google.com/a/finding-marbles.com/spreadsheet/viewform?formkey=dEZZV1hPYWVZUDc2MFNsUEVRdXpMNWc6MQ">Add activity</a>';
 
-//$_lang['INDEX_TEAM_TRANSLATOR_TITLE'] = 'Translation: ';
-//$_lang['INDEX_TEAM_TRANSLATOR_NAME'] = 'Your Name';
-//$_lang['INDEX_TEAM_TRANSLATOR_LINK'] = 'Your URL';
-//$_lang['INDEX_TEAM_TRANSLATOR_IMAGE'] = 'static/images/team/ - send me a picture of you :)';
-//$_lang['INDEX_TEAM_TRANSLATOR_TEXT'] = <<<EOT
-//             Tell us something about you! <a href="https://twitter.com/YourHandle">Twitter</a>!
-//EOT;
+$_lang['INDEX_ABOUT'] = 'Retromat contiene <span class="js_footer_no_of_activities"></span> actividades, permitiendo generar <span class="js_footer_no_of_combinations"></span> combinaciones (<span class="js_footer_no_of_combinations_formula"></span>) y estamos continuamente agregando m&aacute;s.'; // &iquest;Conoces una buena actividad?
+$_lang['INDEX_ABOUT_SUGGEST'] = '&iexcl;Proponla';
 
-$_lang['INDEX_TEAM_CORINNA_TITLE'] = 'Created by ';
-$_lang['INDEX_TEAM_CORINNA_TEXT'] = $_lang['INDEX_MINI_TEAM'] = <<<EOT
-    Corinna llevaba pensando en algo como Retr-O-Mat desde sus años como Scrummaster.
-    Finalmente lo hizo ella misma con la esperanza de que fuera util para otros tambien.
-    ¿Alguna pregunta, sugerencia o simplemente felicitacion?
-    Puedes escribirla a <a href="mailto:corinna@finding-marbles.com">corinna@finding-marbles.com</a> o
-    <a href="https://twitter.com/findingmarbles">contactar con ella en Twitter</a>.
+$_lang['INDEX_TEAM_TRANSLATOR_TITLE'] = 'Traducido por ';
+$_lang['INDEX_TEAM_TRANSLATOR_NAME'][0] = 'Thomas Wallet';
+$_lang['INDEX_TEAM_TRANSLATOR_LINK'][0] = 'http://thomaswallet.blogspot.com.ar';
+$_lang['INDEX_TEAM_TRANSLATOR_IMAGE'][0] = 'static/images/team/thomas_wallet.jpg';
+$_lang['INDEX_TEAM_TRANSLATOR_TEXT'][0] = <<<EOT
+Trabajador &aacute;gil pragm&aacute;tico. Enfermo de las retrospectivas. Puede <a href="https://twitter.com/WalletThomas">seguirlo en Twitter</a>.
+<br><br><br><br>
+EOT;
+$_lang['INDEX_TEAM_TRANSLATOR_NAME'][1] = 'Pedro Serrano';
+$_lang['INDEX_TEAM_TRANSLATOR_LINK'][1] = 'https://twitter.com/pedroserranot';
+$_lang['INDEX_TEAM_TRANSLATOR_IMAGE'][1] = 'static/images/team/pedro_angel_serrano.jpg';
+$_lang['INDEX_TEAM_TRANSLATOR_TEXT'][1] = <<<EOT
+Agile Beleiver y Agente de Cambio, apasionado de las metodolog&iacute;as &aacute;giles (CSM, PSM y ACP). Puede <a href="https://twitter.com/Pedroserranot">seguirlo en Twitter</a>.
+<br> <br>
+Agradecimientos a Mauro Strione, Sebastian Sabato, Mauro Tresguerras, Martin Contuci, Hiroshi Hiromoto, Lucho Salazar y Jos&eacute; Mar&iacute;a Bl&aacute;zquez.
 EOT;
 
-$_lang['PRINT_HEADER'] = 'by Finding-Marbles.com';
+$_lang['INDEX_TEAM_CORINNA_TITLE'] = 'Creado por ';
+$_lang['INDEX_TEAM_CORINNA_TEXT'] = $_lang['INDEX_MINI_TEAM'] = <<<EOT
+Corinna ech&oacute; en falta algo como Retromat en sus a&ntilde;os de Scrummaster.
+Finalmente lo desarroll&oacute; ella misma, esperando que tambi&eacute;n sea de utilidad para otros.
+&iquest;Preguntas, sugerencias o aliento?
+Puede <a href="mailto:corinna@finding-marbles.com">escribirle</a> o <a href="https://twitter.com/findingmarbles">seguirla en Twitter</a>.
+EOT;
 
-$_lang['ACTIVITY_SOURCE'] = 'Fuente:';
-$_lang['ACTIVITY_PREV'] = 'Mostrar otra activada para esta fase';
-$_lang['ACTIVITY_NEXT'] = 'Mostrar otra activada para esta fase';
-$_lang['ACTIVITY_PHOTO_ADD'] = 'Añadir foto';
-$_lang['ACTIVITY_PHOTO_MAIL_SUBJECT'] = 'Photos%20for%20Activity%3A%20ID';
-$_lang['ACTIVITY_PHOTO_MAIL_BODY'] = 'Hi%20Corinna%21%0D%0A%0D%0A[%20]%20Photo%20is%20attached%0D%0A[%20]%20Photo%20is%20online%20at%3A%20%0D%0A%0D%0ABest%2C%0D%0AYour%20Name';
+$_lang['INDEX_TEAM_TIMON_TITLE'] = 'Co-developed by ';
+$_lang['INDEX_TEAM_TIMON_TEXT'] = <<<EOT
+Tanto como Desarrollador, Product Owner, Scrum Master y Agile Coach, Timon ha sido usuario y fan de Retromat durante m&aacute;s de tres a&ntilde;os. Se le ocurrieron bastantes ideas al respecto y en 2016 comenz&oacute; a desarrollar &eacute;l mismo algunas de ellas. Puedes <a href="mailto:timon.fiddike@agile.coach">mandarle un email</a> o
+    <a href="https://twitter.com/TimonFiddike">seguirle en Twitter</a>.
+EOT;
 
-$_lang['ERROR_NO_SCRIPT'] = 'Retr-O-Mat depende estrechamente de JavaScript y no funciona sin el. Por favor, habilita Javascript en tu navegador. Gracias!';
-$_lang['ERROR_MISSING_ACTIVITY'] = 'Lo siento, no se encontro una actividad con ID';
+$_lang['PRINT_HEADER'] = 'por Finding-Marbles.com';
+
+$_lang['ACTIVITY_SOURCE'] = 'Origen:';
+$_lang['ACTIVITY_PREV'] = 'Mostrar otra actividad para esta fase';
+$_lang['ACTIVITY_NEXT'] = 'Mostrar otra actividad para esta fase';
+$_lang['ACTIVITY_PHOTO_ADD'] = 'Agregar foto';
+$_lang['ACTIVITY_PHOTO_MAIL_SUBJECT'] = 'Fotos%20para%20Actividad%3A%20ID';
+$_lang['ACTIVITY_PHOTO_MAIL_BODY'] = 'Hola%20Corinna%21%0D%0A%0D%0A[%20]%20Se%20adjuntan%20fotos%0D%0A[%20]%20Las%20fotos%20estan%20online%20aqui%3A%20%0D%0A%0D%0AAtt.%2C%0D%0ATu%20Nombre';
+$_lang['ACTIVITY_PHOTO_VIEW_PHOTO'] = 'Ver foto';
+$_lang['ACTIVITY_PHOTO_VIEW_PHOTOS'] = 'Ver fotos';
+$_lang['ACTIVITY_PHOTO_BY'] = 'Foto por ';
+
+$_lang['ERROR_NO_SCRIPT'] = 'Retromat se apoya fuertemente sobre Javascript y no puede funcionar sin el. Por favor habilitalo en tu navegador. &iexcl;Gracias!';
+$_lang['ERROR_MISSING_ACTIVITY'] = 'Disculpa, no se encuentra actividad con el ID';
 
 $_lang['POPUP_CLOSE'] = 'Cerrar';
-$_lang['POPUP_IDS_BUTTON'] = 'Mostrar!';
+$_lang['POPUP_IDS_BUTTON'] = 'Mostrar';
 $_lang['POPUP_IDS_INFO']= 'Ejemplo ID: 3-33-20-13-45';
 $_lang['POPUP_SEARCH_BUTTON'] = 'Buscar';
-$_lang['POPUP_SEARCH_INFO']= 'Buscar titulos, resumenes &amp; o descripciones';
-$_lang['POPUP_SEARCH_NO_RESULTS'] = 'Lo siento, no se encontro nada para';
+$_lang['POPUP_SEARCH_INFO']= 'Buscar en t&iacute;tulos, res&uacute;menes &amp; descripciones';
+$_lang['POPUP_SEARCH_NO_RESULTS'] = 'Disculpa, no se encontr&oacute; nada para';
 
 ?>
