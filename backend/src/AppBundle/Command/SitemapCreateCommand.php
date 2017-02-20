145a1baee5945ea90db526f4e049c1a95aee8ee5
Timon Fiddike <timon-git@fiddike.com>
2017-02-20 17:06:39 +0100

    Command explaining how to create the sitemap.

---
+++ backend/src/AppBundle/Command/SitemapCreateCommand.php
@@ -0,0 +1,25 @@
+<?php
+namespace AppBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SitemapCreateCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            // the name of the command (the part after "bin/console")
+            ->setName('retromat:sitemap:create')
+
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Please use the presta:sitemaps:dump command for creating the sitemap.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Please use the presta:sitemaps:dump command for creating the sitemap.');
+    }
+}
