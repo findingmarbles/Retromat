a0fa091ae55023a1d99a82362ec2c3a0483aebe5
Timon Fiddike <timon-git@fiddike.com>
2017-02-26 15:05:05 +0100

    Install and configure database migration tools.

--- backend/app/AppKernel.php
+++ backend/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Presta\SitemapBundle\PrestaSitemapBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
