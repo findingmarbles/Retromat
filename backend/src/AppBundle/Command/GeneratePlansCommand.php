b4178c34a5a33e51945e3eac82c7f5fa7b444796
Timon Fiddike <timon-git@fiddike.com>
2017-03-02 19:20:15 +0100

    Command to generate plans and store them in the database. At the moment, plan titles are filled with dummy values, because I want to do some performance testing before developing further. Finished debugging memory leak: Plan generation executes many database queries and in the dev environment, logging and profiling information cause memory issues. When running in the prod environment, millions of plans can be generated using ~15 MB of RAM. Curious to benchmark this live.

--- backend/src/AppBundle/Command/GeneratePlansCommand.php
+++ backend/src/AppBundle/Command/GeneratePlansCommand.php
@@ -12,18 +12,26 @@ class GeneratePlansCommand extends ContainerAwareCommand
         $this
             // the name of the command (the part after "bin/console")
             ->setName('retromat:generate:plans')
-
             // the short description shown while running "php bin/console list"
             ->setDescription('Generate Plans.')
-
             // the full command description shown when running the command with
             // the "--help" option
-            ->setHelp('This command allows you to generate plans and store them in the database. At the moment, plan titles are filled with dummy values.')
-        ;
+            ->setHelp(
+                'Generate plans and store them in the database. At the moment, plan titles are filled with dummy values.'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if ('prod' !== $this->getContainer()->get('kernel')->getEnvironment()) {
+            $output->writeln(
+                'Plan generation executes many database queries and in the dev environment, logging and profiling information cause memory issues.'
+            );
+            $output->writeln('Please re-run this command in the prod environment by appending: --env-prod');
+
+            return;
+        }
+
         $output->writeln('Generating plans and storing them in the database ...');
 
         $this->getContainer()->get('retromat.plan.plan_generator')->generateAll();
