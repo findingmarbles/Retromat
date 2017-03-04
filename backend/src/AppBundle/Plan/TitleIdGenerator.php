e7d906bdd0e0c9aeae8e3169225b364bf63fe5a5
Timon Fiddike <timon-git@fiddike.com>
2017-03-04 20:56:43 +0100

    Refactor

--- backend/src/AppBundle/Plan/TitleIdGenerator.php
+++ backend/src/AppBundle/Plan/TitleIdGenerator.php
@@ -6,6 +6,7 @@ class TitleIdGenerator
 {
     private $sequenceOfGroups;
     private $groupsOfTerms;
+    private $allCombinations;
 
     public function __construct(array $titleParts)
     {
@@ -35,12 +36,12 @@ class TitleIdGenerator
 
     public function generateIds($sequenceId)
     {
-        $termIdsInCombination = [];
+        $this->allCombinations = [];
 
         foreach ($this->sequenceOfGroups[$sequenceId] as $groupId) {
             if (1 === count($this->groupsOfTerms[$groupId])) {
                 $termId = 0;
-                $termIdsInCombination[] = $termId;
+                $this->allCombinationsAppendSingleTermId($termId);
             }
             // foreach ($this->groupsOfTerms[$groupId] as $termId => $term) {
             // we can skip the loop, as long as each group only has one term:
@@ -50,6 +51,22 @@ class TitleIdGenerator
             // }
         }
 
-        return [$sequenceId.':'.implode('-', $termIdsInCombination)];
+        $combinationIds = [];
+        foreach ($this->allCombinations as $combination) {
+            $combinationIds[] = $sequenceId.':'.implode('-', $combination);
+        }
+
+        return $combinationIds;
+    }
+
+    private function allCombinationsAppendSingleTermId($termId)
+    {
+        if (0 === count($this->allCombinations)) {
+            $this->allCombinations[0] = [$termId];
+        } else {
+            for ($i =0 ; $i < count($this->allCombinations) ; $i++) {
+                $this->allCombinations[$i][] = $termId;
+            }
+        }
     }
 }
