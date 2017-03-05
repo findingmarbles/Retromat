79635ddf1c6bc26e78258832972224bbc6335816
Timon Fiddike <timon-git@fiddike.com>
2017-03-05 20:24:07 +0100

    TDD: DifferentPlansGetDifferentTitles

--- backend/src/AppBundle/Plan/TitleIdChooser.php
+++ backend/src/AppBundle/Plan/TitleIdChooser.php
@@ -1,10 +1,20 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace AppBundle\Plan;
 
 class TitleIdChooser
 {
+    private $sequenceOfGroups;
+
+    private $groupsOfTerms;
+
+    public function __construct(array $titleParts)
+    {
+        $this->sequenceOfGroups = $titleParts['sequence_of_groups'];
+        $this->groupsOfTerms = $titleParts['groups_of_terms'];
+    }
+
     public function chooseTitleId(string $activityIdsString): string
     {
         $activityIds = explode('-', $activityIdsString);
@@ -12,6 +22,18 @@ class TitleIdChooser
             return '';
         }
 
-        return '0:0-0-0';
+        $planNumber = (int)implode('0', $activityIds);
+        mt_srand($planNumber);
+
+        $chosenSequenceId = mt_rand(0, count($this->sequenceOfGroups) - 1);
+        $groupIds = $this->sequenceOfGroups[$chosenSequenceId];
+
+        $chosenTermIds = [];
+        foreach ($groupIds as $groupId) {
+            $groupOfTerms = $this->groupsOfTerms[$groupId];
+            $chosenTermIds[] = mt_rand(0, count($groupOfTerms) - 1);
+        }
+
+        return $chosenSequenceId.':'.implode('-', $chosenTermIds);
     }
 }
--- backend/tests/AppBundle/Plan/TitleIdChooserTest.php
+++ backend/tests/AppBundle/Plan/TitleIdChooserTest.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 namespace tests\AppBundle\Plan;
 
 use AppBundle\Plan\TitleIdChooser;
+use Symfony\Component\Yaml\Yaml;
 
 class TitleIdChooserTest extends \PHPUnit_Framework_TestCase
 {
     public function testChooseTitleIdEmptyUnless5Activities()
     {
-        $chooser = new TitleIdChooser();
+        $titleParts = Yaml::parse(
+            '
+sequence_of_groups:
+    0: [0, 1, 2]
+
+groups_of_terms:
+    0: [Agile, Scrum, Kanban, XP]
+    1: [Retro, Retrospective]
+    2: [Plan, Agenda]
+'
+        );
+        $chooser = new TitleIdChooser($titleParts);
 
         $this->assertEquals('', $chooser->chooseTitleId('1'));
         $this->assertEquals('', $chooser->chooseTitleId('1-2-3-4'));
@@ -18,7 +30,18 @@ class TitleIdChooserTest extends \PHPUnit_Framework_TestCase
 
     public function testChooseTitleIdCorrectFormat()
     {
-        $chooser = new TitleIdChooser();
+        $titleParts = Yaml::parse(
+            '
+sequence_of_groups:
+    0: [0, 1, 2]
+
+groups_of_terms:
+    0: [Agile, Scrum, Kanban, XP]
+    1: [Retro, Retrospective]
+    2: [Plan, Agenda]
+'
+        );
+        $chooser = new TitleIdChooser($titleParts);
 
         $titleId = $chooser->chooseTitleId('1-2-3-4-5');
 
@@ -35,5 +58,30 @@ class TitleIdChooserTest extends \PHPUnit_Framework_TestCase
         foreach ($fragmentIds as $fragmentId) {
             $this->assertTrue(is_numeric($fragmentId));
         }
+    }
+
+    public function testDifferentPlansGetDifferentTitles()
+    {
+        $titleParts = Yaml::parse(
+            '
+sequence_of_groups:
+    0: [0, 1, 2]
+    1: [   1, 2]
+    2: [0, 1   ]
+
+groups_of_terms:
+    0: [Agile, Scrum, Kanban, XP]
+    1: [Retro, Retrospective]
+    2: [Plan, Agenda]
+'
+        );
+        $chooser = new TitleIdChooser($titleParts);
+
+        $titleId1 = $chooser->chooseTitleId('1-2-3-4-5');
+        $titleId2 = $chooser->chooseTitleId('1-2-3-4-6');
+        $this->assertNotEquals($titleId2, $titleId1);
+
+        $titleId2 = $chooser->chooseTitleId('1-2-3-4-7');
+        $this->assertNotEquals($titleId2, $titleId1);
     }
 }
