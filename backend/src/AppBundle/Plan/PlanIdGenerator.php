43134b0fe3ed2fda506f0e15cd1095a19b92cfd7
Timon Fiddike <timon-git@fiddike.com>
2017-03-11 23:34:03 +0100

    TDD: PlanIdGenerator: generate now supports skipping any number of results, while still obeying the specified maximum number of results.

--- backend/src/AppBundle/Plan/PlanIdGenerator.php
+++ backend/src/AppBundle/Plan/PlanIdGenerator.php
@@ -23,18 +23,22 @@ class PlanIdGenerator
     /**
      * @param callable $callback
      * @param int $maxResults
+     * @param int $skip
      */
-    public function generate(callable $callback, int $maxResults = PHP_INT_MAX)
+    public function generate(callable $callback, int $maxResults = PHP_INT_MAX, int $skip = 0)
     {
-        $total = 0;
+        $totalResults = 0;
         foreach ($this->activitiesByPhase[4] as $id4) {
             foreach ($this->activitiesByPhase[3] as $id3) {
                 foreach ($this->activitiesByPhase[2] as $id2) {
                     foreach ($this->activitiesByPhase[1] as $id1) {
                         foreach ($this->activitiesByPhase[0] as $id0) {
-                            if (++$total > $maxResults) {
+                            if ($totalResults >= $maxResults) {
                                 return;
+                            } elseif (0 < $skip) {
+                                --$skip;
                             } else {
+                                ++$totalResults;
                                 call_user_func($callback, $id0.'-'.$id1.'-'.$id2.'-'.$id3.'-'.$id4);
                             }
                         }
--- backend/tests/AppBundle/Plan/PlanIdGeneratorTest.php
+++ backend/tests/AppBundle/Plan/PlanIdGeneratorTest.php
@@ -61,6 +61,41 @@ class PlanIdGeneratorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('6-2-3-4-5', $this->ids[1]);
     }
 
+    public function testGenerateSkip0()
+    {
+        $skip = 0;
+        $maxResults = 2;
+
+        $this->planIdGenerator->generate([$this, 'collect'], $maxResults, $skip);
+
+        $this->assertCount($maxResults, $this->ids);
+        $this->assertEquals('1-2-3-4-5', $this->ids[0]);
+        $this->assertEquals('6-2-3-4-5', $this->ids[1]);
+    }
+
+    public function testGenerateSkip1()
+    {
+        $skip = 1;
+        $maxResults = 2;
+
+        $this->planIdGenerator->generate([$this, 'collect'], $maxResults, $skip);
+
+        $this->assertEquals('6-2-3-4-5', $this->ids[0]);
+        $this->assertEquals('1-7-3-4-5', $this->ids[1]);
+        $this->assertCount($maxResults, $this->ids);
+    }
+
+    public function testGenerateSkip2()
+    {
+        $skip = 2;
+        $maxResults = 1;
+
+        $this->planIdGenerator->generate([$this, 'collect'], $maxResults, $skip);
+
+        $this->assertEquals('1-7-3-4-5', $this->ids[0]);
+        $this->assertCount($maxResults, $this->ids);
+    }
+
     /**
      * @param string $id
      */
