e9d26f5682e6262865a8b18cd1d0964288c60c32
Timon Fiddike <timon-git@fiddike.com>
2017-03-11 22:23:30 +0100

    Refactor PlanIdGeneratorTest

--- backend/tests/AppBundle/Plan/PlanIdGeneratorTest.php
+++ backend/tests/AppBundle/Plan/PlanIdGeneratorTest.php
@@ -8,15 +8,20 @@ use AppBundle\Activity\ActivityByPhase;
 
 class PlanIdGeneratorTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var array
+     */
     private $ids = [];
+
+    /**
+     * @var PlanIdGenerator
+     */
+    private $planIdGenerator;
 
     public function setUp()
     {
         $this->ids = [];
-    }
 
-    public function testGenerateLimit()
-    {
         $activitiesByPhase = [
             0 => [1, 6],
             1 => [2, 7],
@@ -32,36 +37,12 @@ class PlanIdGeneratorTest extends \PHPUnit_Framework_TestCase
         $activitiyByPhase->expects($this->any())
             ->method('getAllActivitiesByPhase')
             ->will($this->returnValue($activitiesByPhase));
-        $planGenerator = new PlanIdGenerator($activitiyByPhase);
-        $limit = 2;
-
-        $planGenerator->generate([$this, 'collect'], $limit);
-
-        $this->assertCount($limit, $this->ids);
-        $this->assertEquals('1-2-3-4-5', $this->ids[0]);
-        $this->assertEquals('6-2-3-4-5', $this->ids[1]);
+        $this->planIdGenerator = new PlanIdGenerator($activitiyByPhase);
     }
 
     public function testGenerateAll()
     {
-        $activitiesByPhase = [
-            0 => [1, 6],
-            1 => [2, 7],
-            2 => [3],
-            3 => [4],
-            4 => [5],
-        ];
-        $activitiyByPhase = $this
-            ->getMockBuilder(ActivityByPhase::class)
-            ->setMethods(['getAllActivitiesByPhase'])
-            ->disableOriginalConstructor()
-            ->getMock();
-        $activitiyByPhase->expects($this->any())
-            ->method('getAllActivitiesByPhase')
-            ->will($this->returnValue($activitiesByPhase));
-        $planGenerator = new PlanIdGenerator($activitiyByPhase);
-
-        $planGenerator->generate([$this, 'collect']);
+        $this->planIdGenerator->generate([$this, 'collect']);
 
         $this->assertEquals('1-2-3-4-5', $this->ids[0]);
         $this->assertEquals('6-2-3-4-5', $this->ids[1]);
@@ -69,6 +50,20 @@ class PlanIdGeneratorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('6-7-3-4-5', $this->ids[3]);
     }
 
+    public function testGenerateLimit()
+    {
+        $limit = 2;
+
+        $this->planIdGenerator->generate([$this, 'collect'], $limit);
+
+        $this->assertCount($limit, $this->ids);
+        $this->assertEquals('1-2-3-4-5', $this->ids[0]);
+        $this->assertEquals('6-2-3-4-5', $this->ids[1]);
+    }
+
+    /**
+     * @param string $id
+     */
     public function collect(string $id)
     {
         $this->ids[] = $id;
