2a7e12ec56d09d4fae4c899defc805d84ba21590
Timon Fiddike <timon-git@fiddike.com>
2017-03-19 12:53:56 +0100

    TDD: RenderEmptyDescriptionUnless5Activities

--- backend/src/AppBundle/Plan/DescriptionRenderer.php
+++ backend/src/AppBundle/Plan/DescriptionRenderer.php
@@ -5,8 +5,16 @@ namespace AppBundle\Plan;
 
 class DescriptionRenderer
 {
-    public function render(): string
+    /**
+     * @param array $activities
+     * @return string
+     */
+    public function render(array $activities): string
     {
+        if (5 !== count($activities)) {
+            return '';
+        }
+
         return '1, 4: Participants write down significant events and order them chronologically , 8: Drill down to the root cause of problems by repeatedly asking \'Why?\', 11, 14';
     }
 }
--- backend/tests/AppBundle/Plan/DescriptionRendererTest.php
+++ backend/tests/AppBundle/Plan/DescriptionRendererTest.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 namespace tests\AppBundle\Plan;
 
+use AppBundle\Entity\Activity;
 use AppBundle\Plan\DescriptionRenderer;
 
 class DescriptionRendererTest extends \PHPUnit_Framework_TestCase
@@ -11,6 +12,30 @@ class DescriptionRendererTest extends \PHPUnit_Framework_TestCase
     {
         $renderer = new DescriptionRenderer();
 
-        $this->assertEquals('1, 4: Participants write down significant events and order them chronologically , 8: Drill down to the root cause of problems by repeatedly asking \'Why?\', 11, 14', $renderer->render());
+        $activities[] = new Activity();
+        $activities[] = new Activity();
+        $activities[] = new Activity();
+        $activities[] = new Activity();
+        $activities[] = new Activity();
+
+        $this->assertEquals('1, 4: Participants write down significant events and order them chronologically , 8: Drill down to the root cause of problems by repeatedly asking \'Why?\', 11, 14', $renderer->render($activities));
+    }
+
+    public function testRenderEmptyDescriptionUnless5Activities()
+    {
+        $renderer = new DescriptionRenderer();
+
+        $activities = [];
+        $this->assertEmpty($renderer->render($activities));
+
+        $activities[] = new Activity();
+        $activities[] = new Activity();
+        $activities[] = new Activity();
+        $activities[] = new Activity();
+        $this->assertEmpty($renderer->render($activities));
+
+        $activities[] = new Activity();
+        $activities[] = new Activity();
+        $this->assertEmpty($renderer->render($activities));
     }
 }
