82e78c4351f1b630efe776a9163727b9ffc252e3
Timon Fiddike <timon-git@fiddike.com>
2017-07-17 18:53:37 +0200

    Deliver json encoded single activity

--- backend/src/AppBundle/Controller/ActivityController.php
+++ backend/src/AppBundle/Controller/ActivityController.php
@@ -5,11 +5,16 @@ namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\View\View;
 
 class ActivityController extends FOSRestController implements ClassResourceInterface
 {
-    public function getAction($slug)
+    public function getAction($id)
     {
+        $repo = $this->get('doctrine.orm.entity_manager')->getRepository('AppBundle:Activity');
+        $activity = $repo->find($id);
+
+        return new View($activity);
     }
 
     public function cgetAction()
