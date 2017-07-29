843c34524b59e28fdb4f28f73ee613d7b5a2b4a8
Timon Fiddike <timon-git@fiddike.com>
2017-07-29 20:30:30 +0200

    Make Symfony Debug Toolbar and Profiler available via SSH Tunnel. The procedure to set up the tunnel is described in a comment at the top of backend/web/app_dev.php and the link to access it is included in the team dashboard.

--- backend/web/app_dev.php
+++ backend/web/app_dev.php
@@ -3,16 +3,17 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read https://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
-// for more information
-//umask(0000);
-
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || PHP_SAPI === 'cli-server')
+// Whitelist includes 185.26.156.32 which belongs to avior, so that
+// app_dev.php can be reached using a tunnel as decribed here:
+// Temporarily add a line to your local /etc/hosts file ...
+// 127.0.0.1 		plans-for-retrospectives.com
+// ... so your browser will make a request to 127.0.0.1 using the correct domain name. Then:
+// Make sure you have a free local port 443 (i.e. no local webserver running on it).
+// Test it by tryning to access https://plans-for-retrospectives.com, there should be a connection error.
+// Then create the tunnel.
+// sudo ssh -N -L 443:plans-for-retrospectives.com:443 retromat@avior.uberspace.de
+// Now you should be able to access https://plans-for-retrospectives.com/app_dev.php
+if (!(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '185.26.156.32']) || PHP_SAPI === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
