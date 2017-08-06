2a3c313f1b7b546319dc590b82325a7d3e47dcb9
Timon Fiddike <timon-git@fiddike.com>
2017-08-06 09:23:32 +0200

    Prepare DB structure that allows for shared activity metadata (duration etc.) across different languages: Install KnpDoctrineBehavioursBundle which includes translatable extension we will use ... composer.phar require knplabs/doctrine-behaviors:~1.4 ... then add to AppKernel, add to config, run tests.

--- backend/app/AppKernel.php
+++ backend/app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
