23ab4321da6f0503428eb39f1deb93226e263d4c
Timon Fiddike <timon-git@fiddike.com>
2017-08-12 01:16:31 +0200

    More PHP7 strict types. Autoformat.

--- backend/src/AppBundle/Activity/ActivityByPhase.php
+++ backend/src/AppBundle/Activity/ActivityByPhase.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace AppBundle\Activity;
 
@@ -7,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 class ActivityByPhase
 {
     private $activityByPhase;
+
     private $entityManager;
 
     /**
@@ -37,11 +39,11 @@ class ActivityByPhase
         $idKey = array_search($id, $this->activityByPhase[$phase]);
 
         // if we are on the last activity of the phase, the next one is the first
-        if ($idKey == count($this->activityByPhase[$phase]) - 1) {
+        if ($idKey == count($this->activityByPhase[$phase])-1) {
             return $this->activityByPhase[$phase][0];
         }
 
-        return $this->activityByPhase[$phase][$idKey + 1];
+        return $this->activityByPhase[$phase][$idKey+1];
     }
 
     public function previousActivityIdInPhase($phase, $id)
@@ -51,10 +53,10 @@ class ActivityByPhase
 
         // if we are on the first activity of the phase, the previous one is the last
         if (0 == $idKey) {
-            return $this->activityByPhase[$phase][count($this->activityByPhase[$phase]) - 1];
+            return $this->activityByPhase[$phase][count($this->activityByPhase[$phase])-1];
         }
 
-        return $this->activityByPhase[$phase][$idKey - 1];
+        return $this->activityByPhase[$phase][$idKey-1];
     }
 
     public function nextIds(array $ids, $id, $phase)
@@ -79,7 +81,9 @@ class ActivityByPhase
     private function lazyInit()
     {
         if (!isset($this->activityByPhase)) {
-            $this->activityByPhase = $this->entityManager->getRepository('AppBundle:Activity')->findAllActivitiesByPhases();
+            $this->activityByPhase = $this->entityManager->getRepository(
+                'AppBundle:Activity'
+            )->findAllActivitiesByPhases();
         }
     }
 }
