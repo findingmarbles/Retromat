6c059d89e59010472dad5c31849d93124c18a901
Timon Fiddike <timon-git@fiddike.com>
2017-08-12 21:05:14 +0200

    Add Entities and DB structure migration that allows for shared activity metadata (duration etc.) across different languages using Translatable trait from KnpDoctrineBehavioursBundle. Keep the previous DB structure as well, so we can avoid having to do a big bang switch and migrate step by step instead. Important, because we still need to find out whether the new structure actually fits our needs.

---
+++ backend/src/AppBundle/Entity/Activity2.php
@@ -0,0 +1,238 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Knp\DoctrineBehaviors\Model as ORMBehaviors;
+
+/**
+ * Activity2
+ *
+ * @ORM\Table(name="activity2")
+ * @ORM\Entity
+ */
+class Activity2
+{
+    use ORMBehaviors\Translatable\Translatable;
+
+    /**
+     * Internal id used by the Doctine ORM.
+     * Property name needs to be "id" for the Translatable trait.
+     *
+     * @var int
+     * @ORM\Column(name="doctrine_id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * $retromatId is the public ID as in http://plans-for-retrospectives.com/?id=123
+     * this differs by -1 from the internal ID in JS code, e.g. all_activities[122]
+     *
+     * @var int
+     * @Assert\Type("integer")
+     * @Assert\NotBlank()
+     * @ORM\Column(name="retromat_id", type="smallint", unique=true)
+     */
+    private $retromatId;
+
+    /**
+     * @var int
+     * @Assert\Type("integer")
+     * @Assert\NotBlank()
+     * @Assert\Range(min = 0, max = 5)
+     * Using `backticks` to avoid mysql reserved keyword https://dev.mysql.com/doc/refman/5.5/en/keywords.html
+     * @ORM\Column(name="`phase`", type="smallint")
+     */
+    private $phase;
+
+    /**
+     * @var string
+     * @Assert\Type("string")
+     * @ORM\Column(name="duration", type="string", length=255, nullable=true)
+     */
+    private $duration;
+
+    /**
+     * @var string
+     * @Assert\Type("string")
+     * @ORM\Column(name="source", type="text", nullable=true)
+     */
+    private $source;
+
+    /**
+     * @var string
+     * @Assert\Type("string")
+     * @ORM\Column(name="more", type="text", nullable=true)
+     */
+    private $more;
+
+    /**
+     * @var string
+     * @Assert\Type("string")
+     * @ORM\Column(name="suitable", type="string", length=255, nullable=true)
+     */
+    private $suitable;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set retromatId
+     *
+     * @param integer $retromatId
+     *
+     * @return Activity2
+     */
+    public function setRetromatId($retromatId)
+    {
+        $this->retromatId = $retromatId;
+
+        return $this;
+    }
+
+    /**
+     * Get retromatId
+     *
+     * @return int
+     */
+    public function getRetromatId()
+    {
+        return $this->retromatId;
+    }
+
+    /**
+     * Set phase
+     *
+     * @param integer $phase
+     *
+     * @return Activity2
+     */
+    public function setPhase($phase)
+    {
+        $this->phase = $phase;
+
+        return $this;
+    }
+
+    /**
+     * Get phase
+     *
+     * @return int
+     */
+    public function getPhase()
+    {
+        return $this->phase;
+    }
+
+    /**
+     * Set duration
+     *
+     * @param string $duration
+     *
+     * @return Activity2
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * Get duration
+     *
+     * @return string
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Set source
+     *
+     * @param string $source
+     *
+     * @return Activity2
+     */
+    public function setSource($source)
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    /**
+     * Get source
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    /**
+     * Set more
+     *
+     * @param string $more
+     *
+     * @return Activity2
+     */
+    public function setMore($more)
+    {
+        $this->more = $more;
+
+        return $this;
+    }
+
+    /**
+     * Get more
+     *
+     * @return string
+     */
+    public function getMore()
+    {
+        return $this->more;
+    }
+
+    /**
+     * Set suitable
+     *
+     * @param string $suitable
+     *
+     * @return Activity2
+     */
+    public function setSuitable($suitable)
+    {
+        $this->suitable = $suitable;
+
+        return $this;
+    }
+
+    /**
+     * Get suitable
+     *
+     * @return string
+     */
+    public function getSuitable()
+    {
+        return $this->suitable;
+    }
+
+    public function __toString()
+    {
+        return (string)$this->retromatId;
+    }
+}
---
+++ backend/src/AppBundle/Entity/Activity2Translation.php
@@ -0,0 +1,117 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Knp\DoctrineBehaviors\Model as ORMBehaviors;
+
+/**
+ * Activity2Translation
+ *
+ * @ORM\Table(name="activity2translation")
+ * @ORM\Entity
+ */
+class Activity2Translation
+{
+    use ORMBehaviors\Translatable\Translation;
+
+    /**
+     * @var string
+     * @Assert\Type("string")
+     * @Assert\NotBlank()
+     * Using `backticks` to avoid mysql reserved keyword https://dev.mysql.com/doc/refman/5.5/en/keywords.html
+     * @ORM\Column(name="`name`", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     * @Assert\Type("string")
+     * @Assert\NotBlank()
+     * @ORM\Column(name="summary", type="string", length=255)
+     */
+    private $summary;
+
+    /**
+     * @var string
+     * @Assert\Type("string")
+     * @Assert\NotBlank()
+     * Using `backticks` to avoid mysql reserved keyword https://dev.mysql.com/doc/refman/5.5/en/keywords.html
+     * @ORM\Column(name="`desc`", type="text")
+     */
+    private $desc;
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Activity2Translation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set summary
+     *
+     * @param string $summary
+     *
+     * @return Activity2Translation
+     */
+    public function setSummary($summary)
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    /**
+     * Get summary
+     *
+     * @return string
+     */
+    public function getSummary()
+    {
+        return $this->summary;
+    }
+
+    /**
+     * Set desc
+     *
+     * @param string $desc
+     *
+     * @return Activity2Translation
+     */
+    public function setDesc($desc)
+    {
+        $this->desc = $desc;
+
+        return $this;
+    }
+
+    /**
+     * Get desc
+     *
+     * @return string
+     */
+    public function getDesc()
+    {
+        return $this->desc;
+    }
+}
