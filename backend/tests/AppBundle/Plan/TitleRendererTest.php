2287227d5d5747efb1fd2767b04eb52546fdd45f
Timon Fiddike <timon-git@fiddike.com>
2017-12-22 11:22:42 +0100

    Server side rendering of non-english plans: de: Added several tests for de case

--- backend/tests/AppBundle/Plan/TitleRendererTest.php
+++ backend/tests/AppBundle/Plan/TitleRendererTest.php
@@ -8,35 +8,45 @@ use Symfony\Component\Yaml\Yaml;
 
 class TitleRendererTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @throws \AppBundle\Twig\Exception\InconsistentInputException
+     */
     public function testRenderDifferentTerms()
     {
         $titleParts = Yaml::parse(file_get_contents(__DIR__.'/TestData/title_minmal.yml'), Yaml::PARSE_KEYS_AS_STRINGS);
         $title = new TitleRenderer($titleParts);
 
-        $this->assertEquals('Agile Retrospective Plan', $title->render('0:0-0-0'));
-        $this->assertEquals('Scrum Retrospective Plan', $title->render('0:1-0-0'));
+        $this->assertEquals('Agile Retrospective Plan', $title->render('0:0-0-0', 'de'));
+        $this->assertEquals('Scrum Retrospective Plan', $title->render('0:1-0-0', 'de'));
     }
 
+    /**
+     * @throws \AppBundle\Twig\Exception\InconsistentInputException
+     */
     public function testRenderDifferentSequences()
     {
         $titleParts = Yaml::parse(file_get_contents(__DIR__.'/TestData/title_minmal.yml'), Yaml::PARSE_KEYS_AS_STRINGS);
         $title = new TitleRenderer($titleParts);
 
-        $this->assertEquals('Retrospective Plan', $title->render('2:0-0'));
-        $this->assertEquals('Agile Retrospective', $title->render('1:0-0'));
+        $this->assertEquals('Retrospective Plan', $title->render('2:0-0', 'de'));
+        $this->assertEquals('Agile Retrospective', $title->render('1:0-0', 'de'));
     }
 
     /**
      * @expectedException \AppBundle\Twig\Exception\InconsistentInputException
+     * @throws \AppBundle\Twig\Exception\InconsistentInputException
      */
     public function testRenderWrongNumberOfIds()
     {
         $titleParts = Yaml::parse(file_get_contents(__DIR__.'/TestData/title_minmal.yml'), Yaml::PARSE_KEYS_AS_STRINGS);
         $title = new TitleRenderer($titleParts);
 
-        $title->render('1:0-0-0');
+        $title->render('1:0-0-0', 'de');
     }
 
+    /**
+     * @throws \AppBundle\Twig\Exception\InconsistentInputException
+     */
     public function testRenderNoSuperfluousWhitespace()
     {
         $yaml = <<<YAML
@@ -47,9 +57,18 @@ groups_of_terms:
     0: ["", "Agile", "Scrum", "Kanban", "XP"]
     1: ["", "Retro", "Retrospective"]
     2: ["Plan", "Agenda"]
+
+de:
+    sequence_of_groups:
+        0: [0, 1, 2]
+    
+    groups_of_terms:
+        0: ["", "Agile", "Scrum", "Kanban", "XP"]
+        1: ["", "Retro", "Retrospective"]
+        2: ["Plan", "Agenda"]
 YAML;
         $titleParts = Yaml::parse($yaml, Yaml::PARSE_KEYS_AS_STRINGS);
         $title = new TitleRenderer($titleParts);
-        $this->assertEquals('Plan', $title->render('0:0-0-0'));
+        $this->assertEquals('Plan', $title->render('0:0-0-0', 'de'));
     }
 }
