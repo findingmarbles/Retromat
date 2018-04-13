05e6d55cfcb0f0e4af7176bdff3b392e8690b220
Corinna <pallas@Corinnas-Air.fritz.box>
2018-04-13 22:56:37 +0200

    Updated number of translated activities

--- index.php
+++ index.php
@@ -235,10 +235,10 @@ var PHASE_ID_TAG = 'phase';
         <img class="header__logo" src="/static/images/logo_white.png" alt="Retromat" title="Retromat"></a>
 
     <select class="languageswitcher" onChange="switchLanguage(this.value)">
-        <option value="de" <?php echo(print_if_selected("de", $lang)); ?> >Deutsch (84 Aktivit&auml;ten)</option>
+        <option value="de" <?php echo(print_if_selected("de", $lang)); ?> >Deutsch (100 Aktivit&auml;ten)</option>
         <option value="en" <?php echo(print_if_selected("en", $lang)); ?> >English (131 activities)</option>
-        <option value="es" <?php echo(print_if_selected("es", $lang)); ?> >Espa&ntilde;ol (97 actividades)</option>
-        <option value="fr" <?php echo(print_if_selected("fr", $lang)); ?> >Fran&ccedil;ais (53 activit&eacute;s)</option>
+        <option value="es" <?php echo(print_if_selected("es", $lang)); ?> >Espa&ntilde;ol (99 actividades)</option>
+        <option value="fr" <?php echo(print_if_selected("fr", $lang)); ?> >Fran&ccedil;ais (59 activit&eacute;s)</option>
         <option value="nl" <?php echo(print_if_selected("nl", $lang)); ?> >Nederlands (101 activiteiten)</option>
         <option value="ru" <?php echo(print_if_selected("ru", $lang)); ?> >Русский (57 упражнений)</option>
     </select>
