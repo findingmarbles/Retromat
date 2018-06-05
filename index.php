077e43e494c345fab286b38dbc9fcc18a9860083
baldauf <baldauf@AdminisMacBook2.fritz.box>
2018-06-05 10:28:09 +0200

    Updated Spanish counter

--- index.php
+++ index.php
@@ -235,7 +235,7 @@ var PHASE_ID_TAG = 'phase';
     <select class="languageswitcher" onChange="switchLanguage(this.value)">
         <option value="de" <?php echo(print_if_selected("de", $lang)); ?> >Deutsch (100 Aktivit&auml;ten)</option>
         <option value="en" <?php echo(print_if_selected("en", $lang)); ?> >English (131 activities)</option>
-        <option value="es" <?php echo(print_if_selected("es", $lang)); ?> >Espa&ntilde;ol (99 actividades)</option>
+        <option value="es" <?php echo(print_if_selected("es", $lang)); ?> >Espa&ntilde;ol (108 actividades)</option>
         <option value="fr" <?php echo(print_if_selected("fr", $lang)); ?> >Fran&ccedil;ais (59 activit&eacute;s)</option>
         <option value="nl" <?php echo(print_if_selected("nl", $lang)); ?> >Nederlands (101 activiteiten)</option>
         <option value="ru" <?php echo(print_if_selected("ru", $lang)); ?> >Русский (57 упражнений)</option>
