919bcbced0592eb1124e058caf02a188fdf97802
findingmarbles <corinna@finding-marbles.com>
2019-10-17 19:06:31 +0200

    Updated EN activities to 139

--- index.php
+++ index.php
@@ -257,7 +257,7 @@ var PHASE_ID_TAG = 'phase';
 
     <select class="languageswitcher" onChange="switchLanguage(this.value)">
         <option value="de" <?php echo(print_if_selected("de", $lang)); ?> >Deutsch (137 Aktivit&auml;ten)</option>
-        <option value="en" <?php echo(print_if_selected("en", $lang)); ?> >English (137 activities)</option>
+        <option value="en" <?php echo(print_if_selected("en", $lang)); ?> >English (139 activities)</option>
         <option value="es" <?php echo(print_if_selected("es", $lang)); ?> >Espa&ntilde;ol (114 actividades)</option>
         <option value="fr" <?php echo(print_if_selected("fr", $lang)); ?> >Fran&ccedil;ais (59 activit&eacute;s)</option>
         <option value="nl" <?php echo(print_if_selected("nl", $lang)); ?> >Nederlands (101 activiteiten)</option>
