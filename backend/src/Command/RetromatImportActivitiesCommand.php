41721424d73533fd10a58077788c59b1a09fd294
Christin Gruber <c.gruber@touchdesign.de>
2021-09-15 21:13:56 +0200

    Add import Command

--- backend/src/Command/RetromatImportActivitiesCommand.php
+++ backend/src/Command/RetromatImportActivitiesCommand.php
@@ -2,41 +2,40 @@
 
 namespace App\Command;
 
+use App\Model\Importer\Activity\ActivityImporter;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class RetromatImportActivitiesCommand extends Command
 {
-    protected static $defaultName = 'retromat:import:activities';
-    protected static $defaultDescription = 'Add a short description for your command';
+    private const DESCRIPTION = 'This command allows you to import activities. Activities not yet existing in the DB are created, existing activities are updated.';
+
+    private ActivityImporter $activityImporter;
+
+    public function __construct(ActivityImporter $activityImporter)
+    {
+        parent::__construct();
+
+        $this->activityImporter = $activityImporter;
+    }
 
     protected function configure(): void
     {
-        $this
-            ->setDescription(self::$defaultDescription)
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
+        $this->setDescription(self::DESCRIPTION);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        try {
+            $this->activityImporter->import();
+        } catch (\Exception $exception) {
+            $io->error(sprintf('Import failed: "%s"', $exception->getMessage()));
+            return 1;
         }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
         return 0;
     }
