5b678a2af48d37b3ef75c2b41bdc79dbfbe27e95
Christin Gruber <c.gruber@touchdesign.de>
2021-09-19 14:57:33 +0200

    Add RobotsController

--- backend/src/Controller/RobotsController.php
+++ backend/src/Controller/RobotsController.php
@@ -9,12 +9,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class RobotsController extends AbstractController
 {
     /**
-     * @Route("/robots", name="robots")
+     * @Route("/robots.txt", host="retromat.org")
      */
-    public function index(): Response
+    public function robotsAllow(): Response
     {
-        return $this->render('robots/index.html.twig', [
-            'controller_name' => 'RobotsController',
-        ]);
+        $response = new Response(
+            <<<EOT
+            User-agent: *
+            Disallow:
+            Crawl-delay: 1
+            
+            Sitemap: https://retromat.org/sitemap.xml
+            EOT
+        );
+        $response->headers->set('content-type', 'text/plain; charset=UTF-8');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/robots.txt")
+     */
+    public function robotsDisallow(): Response
+    {
+        $response = new Response(
+            <<<EOT
+            User-agent: *
+            Disallow: /
+            EOT
+        );
+        $response->headers->set('content-type', 'text/plain; charset=UTF-8');
+
+        return $response;
     }
 }
