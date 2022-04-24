6797c1bbde46121ffc84c9d32eacefd8a015af83
Christin Gruber <c.gruber@touchdesign.de>
2022-04-24 10:59:14 +0200

    Solve package conflicts and old deps for deprecated swiftmailer package

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -14,7 +14,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
