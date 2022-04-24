49ab5d98dd7caefe9a9d4b858c1705e8a42ea1d3
Christin Gruber <c.gruber@touchdesign.de>
2022-04-24 10:04:16 +0200

    Replace swiftmailer with symfony/mailer and update env vars

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -3,7 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
