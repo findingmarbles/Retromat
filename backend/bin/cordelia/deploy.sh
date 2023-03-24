321761342e90b487bab244c53e505a75d4c2e9cb
Timon Fiddike <timon-git@fiddike.com>
2023-03-24 14:37:47 +0100

    clear uberspace RAM as well

--- backend/bin/cordelia/deploy.sh
+++ backend/bin/cordelia/deploy.sh
@@ -8,4 +8,5 @@ backend/bin/travis-ci/generate-templates-from-retromat-v1.sh
 php backend/bin/console cache:clear --no-warmup --env=prod
 redis-cli -s /home/retro2/.redis/sock FLUSHALL
 php backend/bin/console cache:warmup --env=prod
+uberspace tools restart php
 curl --silent --show-error --insecure https://retromat.org/en/ -o /dev/null
