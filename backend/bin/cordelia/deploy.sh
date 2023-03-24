4f801dd649de18bdc582fea21ecf66fe3ed37dfa
Timon Fiddike <timon-git@fiddike.com>
2023-03-24 14:12:54 +0100

    Prepare post-travis deployment

--- backend/bin/cordelia/deploy.sh
+++ backend/bin/cordelia/deploy.sh
@@ -2,12 +2,10 @@
 
 cd /var/www/virtual/retro2/retromat-deployments/retromat.git/
 git pull origin master
-
-cd /var/www/virtual/retro2/retromat-deployments/retromat.git/backend/
-composer install
-php bin/console doctrine:migrations:migrate --no-interaction
-bin/travis-ci/generate-templates-from-retromat-v1.sh
-php bin/console cache:clear --no-warmup --env=prod
+composer install --working-dir=backend
+php backend/bin/console doctrine:migrations:migrate --no-interaction
+backend/bin/travis-ci/generate-templates-from-retromat-v1.sh
+php backend/bin/console cache:clear --no-warmup --env=prod
 redis-cli -s /home/retro2/.redis/sock FLUSHALL
-php bin/console cache:warmup --env=prod
+php backend/bin/console cache:warmup --env=prod
 curl --silent --show-error --insecure https://retromat.org/en/ -o /dev/null
