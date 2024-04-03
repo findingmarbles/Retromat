c5ff408cbad1ed68ea56436d7342ecb6c465ba1d
Timon Fiddike <timon-git@fiddike.com>
2024-04-03 16:27:32 +0200

    deploy script now includes instructions on how to solve composer trouble and combines all cache clear / warmup operations at the end

--- backend/bin/cordelia/deploy.sh
+++ backend/bin/cordelia/deploy.sh
@@ -8,27 +8,29 @@ git pull origin master
 
 # install libs as specified in repo
 composer install --working-dir=backend
+## IF composer fails, try this combo:
+# composer install --no-plugins --no-scripts --working-dir=backend
+# composer install --working-dir=backend
+## IF composer still fails, try this 2x, then the combo above:
+# rm -rf backend/var/cache/*
 
 # adapt database as specified in repo
 php backend/bin/console doctrine:migrations:migrate --no-interaction
 
 # (re-) generate templates from index.php
-index_deploy-from-php-to-twig.sh
-
-# clear activities from RAM
-redis-cli -s /home/retro2/.redis/sock FLUSHALL
-
-# clear and warmup compiled code, templates etc. from disk
-php backend/bin/console cache:clear --no-warmup --env=prod
-php backend/bin/console cache:warmup --env=prod
-
-# clear and warmup compiled code, templates etc. from RAM
-uberspace tools restart php
-
-# warmup RAM loading a page via http
-curl --silent --show-error --insecure https://retromat.org/en/ -o /dev/null
+sh index_deploy-from-php-to-twig.sh
 
 # make sitemap files available via symlinks
 cd /var/www/virtual/retro2/retromat-deployments/retromat.git/backend/public/
 rm sitemap.*
 ln -s ../../../../retromat-sitemaps/* .
+
+# clear compiled code, templates, DB cache etc. from disk, Redis RAM, PHP RAM
+php backend/bin/console cache:clear --no-warmup --env=prod
+redis-cli -s /home/retro2/.redis/sock FLUSHALL
+uberspace tools restart php
+
+# warm up disk part 1
+php backend/bin/console cache:warmup --env=prod
+# warm up disk part 2, Redis RAM, PHP RAM
+curl --silent --show-error --insecure https://retromat.org/en/ -o /dev/null
