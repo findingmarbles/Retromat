cb1197e259fda4b8aa445cf4972b4aee4ec59131
Timon Fiddike <timon-git@fiddike.com>
2025-07-05 14:19:43 +0000

    phpstan finds many issues, need to expore this more before activating itphpstan finds many issues, need to expore this more before activating it

--- backend/quality-checks-in-docker.sh
+++ backend/quality-checks-in-docker.sh
@@ -8,3 +8,6 @@ fi
 
 php vendor/bin/php-cs-fixer fix --config=.php-cs-fixer.dist.php
 php vendor/bin/parallel-lint --exclude /.git --exclude /app/backend/vendor --exclude /app/backend/var /app/backend/
+
+# finds many issues, need to expore this more before activating it
+# vendor/bin/phpstan --memory-limit=2g  analyse src tests
