e560979217ac8a6447ec64cc889f1fdf56595d16
Timon Fiddike <timon-git@fiddike.com>
2025-07-05 12:58:49 +0000

    wrapper for qualitity tools (php-cs right now, others to follow)

---
+++ backend/quality-checks-in-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ ! -d "/app/" ]; then
+    echo 'This is intended to be run in the docker container, like this:'
+    echo 'docker exec -it retromat-php-fpm-1 sh -c "cd /app/backend && ./quality-checks-in-docker.sh"'
+    exit 1
+fi
+
+php vendor/bin/php-cs-fixer fix --config=.php-cs-fixer.dist.php
